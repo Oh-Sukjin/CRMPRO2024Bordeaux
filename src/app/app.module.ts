f86bb0720687058c625b042d6a7321c848924dc0
Sukjin <osj.informatique@gmail.com>
2024-02-19 14:01:24 +0100

    1st commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { SidebarComponent } from './core/sidebar/sidebar.component';
+import { HeaderComponent } from './core/header/header.component';
+import { EnumComponent } from './core/enum/enum.component';
+import { ModelsComponent } from './core/models/models.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    SidebarComponent,
+    HeaderComponent,
+    EnumComponent,
+    ModelsComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/core/enum/enum.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-enum',
+  templateUrl: './enum.component.html',
+  styleUrl: './enum.component.css'
+})
+export class EnumComponent {
+
+}
